adf469b9395c5a4b5d67ba4a24efb98cdc25112b
CristelAlm <cristelkam@gmail.com>
2020-10-05 17:27:38 -0500

    version 2

---
+++ scripts.php
@@ -0,0 +1,19 @@
+
+<meta charset="utf-8">
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="librerias/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="librerias/fontawesome/css/font-awesome.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<script src="librerias/jquery.min.js"></script>
+<script src="librerias/bootstrap/popper.min.js"></script>
+<script src="librerias/bootstrap/bootstrap.min.js"></script>
+<script src="librerias/datatable/jquery.dataTables.min.js"></script>
+<script src="librerias/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="librerias/alertify/alertify.js"></script>
